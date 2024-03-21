8c36a10e73241fe88edbc2f5c76f1d6e1a6444a8
Shrirang.E <shrirang.ekbote04@gmail.com>
2024-03-21 21:19:59 +1300

    forgot to comment so commented and made a new to save the date so it can be edited

--- src/main/java/nz/ac/auckland/se281/Venuestore.java
+++ src/main/java/nz/ac/auckland/se281/Venuestore.java
@@ -2,15 +2,28 @@ package nz.ac.auckland.se281;
 
 public class Venuestore {
 
+  //initializing variables required in the entire class
   public String venueName;
   public String venueCode;
   public int capacity;
   public int hireFee;
+  public String strDate = "TODO";
 
+  //constructor for the class
   public Venuestore(String venueName, String venueCode, int capacity, int hireFee) {
     this.venueName = venueName;
     this.venueCode = venueCode;
     this.capacity = capacity;
     this.hireFee = hireFee;
+  }
+
+  //method to set or change the date of the venue
+  public void setstrDate(String strDate) {
+    this.strDate = strDate;
+  }
+  
+  //method to get the date of the venue
+  public String getstrDate() {
+    return strDate;
   }
 }
--- src/main/java/nz/ac/auckland/se281/bookingstore.java
+++ src/main/java/nz/ac/auckland/se281/bookingstore.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 
 public class bookingstore {
 
+  //initializing variables required in the entire class
   public String bookRef;
   public String Code;
   public LocalDate date;
   public String numAttend;
   public String email;
 
+  //constructor for the class
   public bookingstore(String bookRef, String venueCode, LocalDate date, String numAttend, String email) {
     this.bookRef = bookRef;
     this.Code = venueCode;
